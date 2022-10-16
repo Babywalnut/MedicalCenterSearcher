a1ecdb98c97fb69316791f600bb9f215992f7729
babywalnut <anfuza@naver.com>
2022-10-16 22:50:44 +0900

    #10 APIKey 삭제

---
+++ MedicalCenterSearcher/MedicalCenterSearcher/CenterInfo/Components/CenterInfoContentView.swift
@@ -0,0 +1,20 @@
+//
+//  CenterInfoContentView.swift
+//  MedicalCenterSearcher
+//
+//  Created by 김민성 on 2022/10/16.
+//
+
+import UIKit
+
+class CenterInfoContentView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ MedicalCenterSearcher/MedicalCenterSearcher/Resources/APIKey.swift
@@ -0,0 +1,8 @@
+//
+//  APIKey.swift
+//  MedicalCenterSearcher
+//
+//  Created by 김민성 on 2022/10/16.
+//
+
+import Foundation
