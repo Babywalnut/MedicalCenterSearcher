7b8d5b22463fe8dd6346b2e9ba48dbe38c919251
babywalnut <anfuza@naver.com>
2022-10-16 13:14:11 +0900

    #8 CenterListTableView UI 구현

--- MedicalCenterSearcher/MedicalCenterSearcher/CenterList/Components/CenterList/CenterListTableView.swift
+++ MedicalCenterSearcher/MedicalCenterSearcher/CenterList/Components/CenterList/CenterListTableView.swift
@@ -11,4 +11,20 @@ import SnapKit
 import Then
 
 class CenterListTableView: UITableView {
+
+  override init(frame: CGRect, style: UITableView.Style) {
+    super.init(frame: frame, style: style)
+
+    configure()
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  private func configure() {
+    self.backgroundColor = .systemBackground
+    self.register(CenterListCell.self, forCellReuseIdentifier: CenterListCell.identifier)
+    self.separatorStyle = .singleLine
+  }
 }
